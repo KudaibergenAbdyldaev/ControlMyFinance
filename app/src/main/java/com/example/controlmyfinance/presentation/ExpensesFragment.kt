9e4257070eac5890874110c7a3ef72fc9364e690
KudaibergenAbdyldaev <abdyldaevk02@gmail.com>
2022-06-19 10:30:07 +0600

    init design and added clean architecture layers

--- app/src/main/java/com/example/controlmyfinance/presentation/ExpensesFragment.kt
+++ app/src/main/java/com/example/controlmyfinance/presentation/ExpensesFragment.kt
@@ -1,22 +1,18 @@
-package com.example.controlmyfinance
+package com.example.controlmyfinance.presentation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.controlmyfinance.databinding.FragmentFirstBinding
+import com.example.controlmyfinance.databinding.FragmentExpensesBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class ExpensesFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentExpensesBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,7 +20,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentExpensesBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,9 +28,6 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/controlmyfinance/presentation/ProfitFragment.kt
@@ -0,0 +1,34 @@
+package com.example.controlmyfinance.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.controlmyfinance.databinding.FragmentProfitBinding
+
+
+class ProfitFragment : Fragment() {
+
+    private var _binding: FragmentProfitBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentProfitBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
